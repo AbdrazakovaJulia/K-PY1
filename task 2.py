52614d141aa494f00312c6f45ef5cede9db4c8d0
AbdrazakovaJulia <115394351+AbdrazakovaJulia@users.noreply.github.com>
2022-10-26 19:47:06 +0300

    Тема 4, лабораторная работа 4

---
+++ task 2.py
@@ -0,0 +1,17 @@
+
+def get_count_char(str_):
+    import re
+    spis = re.sub("[\n| !| .| ,]", "", main_str)
+    lst = tuple(spis.lower())
+    lst = [line.rstrip() for line in lst]
+    dict = {symbol:lst.count(symbol) for symbol in lst}
+    for char in lst:
+        return dict
+     # TODO посчитать количество каждой буквы в строке в аргументе str_
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
