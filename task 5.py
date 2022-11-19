6235e11885cc8a5fd66158a3b26cbad1df5a4c8f
AbdrazakovaJulia <115394351+AbdrazakovaJulia@users.noreply.github.com>
2022-11-19 11:17:56 +0300

    Update task 5.py

--- task 5.py
+++ task 5.py
@@ -4,7 +4,7 @@
 def get_random_password() -> str:
     var_ = string.ascii_uppercase + string.ascii_lowercase + string.digits
     for _ in var_:
-        n = 8
+        n = 15
         rand = random.sample(var_, n)
     return "".join(rand)
 print(get_random_password())
