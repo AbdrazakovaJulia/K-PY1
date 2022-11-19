b51a044d1a057dfdee379a3ea6a5889afafbbefd
AbdrazakovaJulia <115394351+AbdrazakovaJulia@users.noreply.github.com>
2022-11-19 13:19:44 +0300

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -10,4 +10,4 @@ def get_unique_list_numbers(start=-10, stop=10, n=15) -> list[int]:
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
-print(len(list_unique_numbers)) == len(set(list_unique_numbers))
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
