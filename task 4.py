f05e928bc89882d9dd2160322c5630975991bf2b
AbdrazakovaJulia <115394351+AbdrazakovaJulia@users.noreply.github.com>
2022-12-05 21:07:07 +0300

    Update task 4.py

--- task 4.py
+++ task 4.py
@@ -3,14 +3,14 @@ import json
 
 INPUT_FILE = "input.csv"
 
-def csv_to_list_dict(INPUT_FILE, sign=',') -> list[dict]:
+def csv_to_list_dict(INPUT_FILE, delimiter=',') -> list[dict]:
     listdict = []
     listrow = []
     with open(INPUT_FILE, 'r') as f:
         pop = f.readlines()
-        zap = pop[0].rstrip().split(sign)
+        zap = pop[0].rstrip().split(delimiter)
         for line in pop[1:]:
-            listrow.append(line.rstrip().split(sign))
+            listrow.append(line.rstrip().split(delimiter))
         for row in listrow:
             elem = {k: v for k, v in zip(zap, row)}
             listdict.append(elem)
